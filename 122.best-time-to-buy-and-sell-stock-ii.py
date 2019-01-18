5d2d7de292226120d8e1d810e83318791853e1d1
Mark Lee <mhlee@ucsd.edu>
2019-01-18 12:28:48 -0800

    Update 122.best-time-to-buy-and-sell-stock-ii.py submission

--- 122.best-time-to-buy-and-sell-stock-ii.py
+++ 122.best-time-to-buy-and-sell-stock-ii.py
@@ -83,3 +83,19 @@ class Solution(object):
         profit += prices[len(prices) - 1] - buy_price
 
         return profit
+
+# Single pass solution
+# Just keep adding profits at every step
+#
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/solution/
+#
+# class Solution {
+#     public int maxProfit(int[] prices) {
+#         int maxprofit = 0;
+#         for (int i = 1; i < prices.length; i++) {
+#             if (prices[i] > prices[i - 1])
+#                 maxprofit += prices[i] - prices[i - 1];
+#         }
+#         return maxprofit;
+#     }
+# }
